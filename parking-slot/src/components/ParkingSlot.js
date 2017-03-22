032a74b5482fa6cc576666c045833b4a21bc6530
Aki Salmi <aki.salmi@iki.fi>
2017-03-22 15:13:43 +0200

    continue with this

---
+++ parking-slot/src/components/ParkingSlot.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ParkingSlot extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <button>Hi!</button>
+            </div>
+        )
+    }
+}
+
+export default ParkingSlot
